370d4bec065a168c27456abcd404b76090940938
escobar-west <victorv.suriel@gmail.com>
2023-08-26 15:43:39 -0400

    added ref pricer

--- rlib/py_pricer.py
+++ rlib/py_pricer.py
@@ -1,4 +1,23 @@
 import polars as pl
+import numpy as np
+from scipy import special
 
-def ref_pricer(df: pl.DataFrame) -> pl.DataFrame:
-    return pl
+
+def ref_pricer(df: pl.DataFrame, rate: float) -> pl.DataFrame:
+    rate = pl.lit(rate)
+    var_factor = df["sigma"] * df["maturity"].sqrt()
+    d1 = (
+        df["maturity"] * (rate + df["sigma"] ** 2 / 2)
+        + (df["asset_price"] / df["strike"]).log()
+    ) / var_factor
+    d2 = d1 - var_factor
+    df = df.with_columns(
+        option_price=norm_cdf(d1) * pl.col("asset_price")
+        - norm_cdf(d2) * pl.col("strike") * (-rate * pl.col("maturity")).exp()
+    )
+
+    return df
+
+
+def norm_cdf(x: pl.Series):
+    return (1 + special.erf(x / np.sqrt(2))) / 2
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,10 @@
 mod rust_pricer;
-use polars::prelude::DataFrame;
 use pyo3::prelude::*;
 use pyo3_polars::{error::PyPolarsErr, PyDataFrame};
 
 #[pyfunction]
 fn mc_pricer(df: PyDataFrame) -> PyResult<PyDataFrame> {
-    let rdf: DataFrame = df.into();
-    let rdf = rust_pricer::mc_pricer(rdf).map_err(PyPolarsErr::from)?;
+    let rdf = rust_pricer::mc_pricer(df.into()).map_err(PyPolarsErr::from)?;
     Ok(PyDataFrame(rdf))
 }
 
