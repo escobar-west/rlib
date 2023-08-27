b5a7bf39c28021c0e2e9d01967879b4bc0eb9701
escobar-west <victorv.suriel@gmail.com>
2023-08-27 14:25:39 -0400

    refactor

--- src/rust_pricer.rs
+++ src/rust_pricer.rs
@@ -1,18 +1,14 @@
 use polars::prelude::*;
 use rand::{thread_rng, Rng};
 use rand_distr::StandardNormal;
-use rayon::prelude::*;
 
 pub fn mc_pricer(df: DataFrame, n_sims: i32) -> PolarsResult<DataFrame> {
     let mut rng = thread_rng();
-    let mut accum_res = Vec::with_capacity(df.height()).resize(df.height(), 0f64);
     for _ in 0..n_sims {
-        let z_vals: Series = (&mut rng)
-            .sample_iter::<f64, _>(StandardNormal)
+        let _z: Vec<f64> = (&mut rng)
+            .sample_iter(StandardNormal)
             .take(df.height())
             .collect();
-        let rand_walk = col("sigma") * col("maturity") * z_vals.lit();
     }
-    let rand_walk = (col("sigma") * col("maturity")).alias("rand_walk");
-    df.lazy().with_column(rand_walk).collect()
+    Ok(df)
 }
