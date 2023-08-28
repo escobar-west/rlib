22eda11d25532d396058c6bd722b4a4ccec44c0d
escobar-west <victorv.suriel@gmail.com>
2023-08-27 23:03:22 -0400

    more rust

--- src/rust_pricer.rs
+++ src/rust_pricer.rs
@@ -2,13 +2,24 @@ use polars::prelude::*;
 use rand::{thread_rng, Rng};
 use rand_distr::StandardNormal;
 
-pub fn mc_pricer(df: DataFrame, n_sims: i32) -> PolarsResult<DataFrame> {
+pub fn mc_pricer(df: DataFrame, rate: f64, n_sims: i32) -> PolarsResult<DataFrame> {
+    let n_assets = df.height();
     let mut rng = thread_rng();
+    let mut avg_price = Vec::with_capacity(n_assets).resize(n_assets, 0.0f64);
+    let df = df.lazy();
     for _ in 0..n_sims {
-        let _z: Vec<f64> = (&mut rng)
-            .sample_iter(StandardNormal)
-            .take(df.height())
+        let z: Series = (&mut rng)
+            .sample_iter::<f64, _>(StandardNormal)
+            .take(n_assets)
             .collect();
+        let rand_walk = col("sigma") * col("maturity") * z.lit();
+        let mean_drift: Expr = (rate.lit() - 0.5.lit() * col("sigma").pow(2)) * col("maturity");
+        let paths = col("asset_price") * (mean_drift + rand_walk).exp();
+        let payoff = ((-rate).lit() * col("maturity")).exp()
+            * (paths - col("strike")).clip_min(AnyValue::Float64(0.0));
+        let output = df.select(&[payoff.alias("payoff")]).collect();
+
+        return output;
     }
-    Ok(df)
+    df.collect()
 }
