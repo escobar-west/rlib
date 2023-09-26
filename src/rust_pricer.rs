a046067970af0800dadcac9f8357672479c587ce
escobar-west <victorv.suriel@gmail.com>
2023-09-26 01:11:32 -0400

    implement rust mc pricer, needs optimization

--- src/rust_pricer.rs
+++ src/rust_pricer.rs
@@ -5,21 +5,19 @@ use rand_distr::StandardNormal;
 pub fn mc_pricer(df: DataFrame, rate: f64, n_sims: i32) -> PolarsResult<DataFrame> {
     let n_assets = df.height();
     let mut rng = thread_rng();
-    let mut avg_price = Vec::with_capacity(n_assets).resize(n_assets, 0.0f64);
+    let mut avg_price = 0.0.lit();
     let df = df.lazy();
     for _ in 0..n_sims {
         let z: Series = (&mut rng)
             .sample_iter::<f64, _>(StandardNormal)
             .take(n_assets)
             .collect();
-        let rand_walk = col("sigma") * col("maturity") * z.lit();
+        let rand_walk = col("sigma") * col("maturity").sqrt() * z.lit();
         let mean_drift: Expr = (rate.lit() - 0.5.lit() * col("sigma").pow(2)) * col("maturity");
         let paths = col("asset_price") * (mean_drift + rand_walk).exp();
         let payoff = ((-rate).lit() * col("maturity")).exp()
             * (paths - col("strike")).clip_min(AnyValue::Float64(0.0));
-        let output = df.select(&[payoff.alias("payoff")]).collect();
-
-        return output;
+        avg_price = avg_price + payoff / (n_sims as f32).lit();
     }
-    df.collect()
+    df.select(&[avg_price.alias("price")]).collect()
 }
