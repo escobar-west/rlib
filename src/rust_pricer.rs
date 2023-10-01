fb18e75c5541eb29a330f05a44224abd9820c448
escobar-west <victorv.suriel@gmail.com>
2023-09-30 21:46:37 -0400

    refactor

--- src/rust_pricer.rs
+++ src/rust_pricer.rs
@@ -3,47 +3,112 @@ use rand::{thread_rng, Rng};
 use rand_distr::StandardNormal;
 use rayon::prelude::*;
 
-pub fn rust_mc_pricer(mut df: DataFrame, rate: f64, n_sims: i32) -> PolarsResult<DataFrame> {
+pub fn rust_mc_pricer(mut df: DataFrame, rate: f64, n_paths: i32) -> PolarsResult<DataFrame> {
     let n_assets = df.height();
-    let mut rng = thread_rng();
-    let sigma: Vec<f64> = df
-        .column("sigma")?
-        .f64()?
-        .into_iter()
-        .map(Option::unwrap)
-        .collect();
-    let strike: Vec<f64> = df
-        .column("strike")?
-        .f64()?
-        .into_iter()
-        .map(Option::unwrap)
-        .collect();
-    let asset_price: Vec<f64> = df
-        .column("asset_price")?
-        .f64()?
-        .into_iter()
-        .map(Option::unwrap)
-        .collect();
-    let maturity: Vec<f64> = df
-        .column("maturity")?
-        .f64()?
-        .into_iter()
-        .map(Option::unwrap)
-        .collect();
+    let sigma = get_as_vecf64(&df, "sigma")?;
+    let strike = get_as_vecf64(&df, "strike")?;
+    let asset_price = get_as_vecf64(&df, "asset_price")?;
+    let maturity = get_as_vecf64(&df, "maturity")?;
     let sqrt_maturity: Vec<f64> = maturity.iter().map(|x| x.sqrt()).collect();
     // mean_drift = (rate - 0.5 * sigma**2) * maturity
-    let mean_drift: Vec<f64> = sigma
-        .iter()
-        .zip(maturity.iter())
+    let mean_drift: Vec<f64> = std::iter::zip(sigma.iter(), maturity.iter())
         .map(|(sig, mat)| mat * (rate - 0.5 * sig * sig))
         .collect();
     // discount_rate = ((-rate).lit() * col("maturity")).exp()
     let discount_rate: Vec<f64> = maturity.iter().map(|mat| (-rate * mat).exp()).collect();
-    // calc_buffer and option_prices are buffers to store calculations through sims
+    let mut option_prices = run_mc_sim(
+        n_assets,
+        n_paths,
+        &sigma,
+        &strike,
+        &asset_price,
+        &sqrt_maturity,
+        &mean_drift,
+        &discount_rate,
+    );
+    // normalize by n_paths
+    for price in option_prices.iter_mut() {
+        *price /= n_paths as f64;
+    }
+    let mut output: Series = option_prices.into_iter().collect();
+    output.rename("option_price");
+    df.with_column(output)?;
+    Ok(df)
+}
+
+pub fn rust_par_mc_pricer(mut df: DataFrame, rate: f64, n_paths: i32) -> PolarsResult<DataFrame> {
+    let n_assets = df.height();
+    let sigma = get_as_vecf64(&df, "sigma")?;
+    let strike = get_as_vecf64(&df, "strike")?;
+    let asset_price = get_as_vecf64(&df, "asset_price")?;
+    let maturity = get_as_vecf64(&df, "maturity")?;
+    let sqrt_maturity: Vec<f64> = maturity.iter().map(|x| x.sqrt()).collect();
+    // mean_drift = (rate - 0.5 * sigma**2) * maturity
+    let mean_drift: Vec<f64> = std::iter::zip(sigma.iter(), maturity.iter())
+        .map(|(sig, mat)| mat * (rate - 0.5 * sig * sig))
+        .collect();
+    // discount_rate = ((-rate).lit() * col("maturity")).exp()
+    let discount_rate: Vec<f64> = maturity.iter().map(|mat| (-rate * mat).exp()).collect();
+    let n_cores = std::thread::available_parallelism()?.get() as i32;
+    let paths_per_core = n_paths / n_cores;
+    let mut option_prices = (0..n_cores)
+        .into_par_iter()
+        .map(|_| {
+            // run one mc sim per core
+            run_mc_sim(
+                n_assets,
+                paths_per_core,
+                &sigma,
+                &strike,
+                &asset_price,
+                &sqrt_maturity,
+                &mean_drift,
+                &discount_rate,
+            )
+        })
+        .reduce(
+            || vec![0.0; n_assets],
+            |a: Vec<f64>, b: Vec<f64>| {
+                std::iter::zip(a.iter(), b.iter())
+                    .map(|(elem_a, elem_b)| elem_a + elem_b)
+                    .collect()
+            },
+        );
+    // normalize by number of paths that actually ran
+    for price in option_prices.iter_mut() {
+        *price /= (paths_per_core * n_cores) as f64;
+    }
+    let mut output: Series = option_prices.into_iter().collect();
+    output.rename("option_price");
+    df.with_column(output)?;
+    Ok(df)
+}
+
+fn get_as_vecf64(df: &DataFrame, col: &str) -> PolarsResult<Vec<f64>> {
+    Ok(df
+        .column(col)?
+        .f64()?
+        .into_iter()
+        .map(Option::unwrap)
+        .collect())
+}
+
+#[allow(clippy::too_many_arguments)]
+fn run_mc_sim(
+    n_assets: usize,
+    n_paths: i32,
+    sigma: &[f64],
+    strike: &[f64],
+    asset_price: &[f64],
+    sqrt_maturity: &[f64],
+    mean_drift: &[f64],
+    discount_rate: &[f64],
+) -> Vec<f64> {
+    let mut rng = thread_rng();
     let mut calc_buffer: Vec<f64> = vec![0.0; n_assets];
-    let mut option_prices: Vec<f64> = vec![0.0; n_assets];
-    for _ in 0..n_sims {
-        // fill in rng values for single sim
+    let mut price_buffer: Vec<f64> = vec![0.0; n_assets];
+    for _ in 0..n_paths {
+        // fill in rng values for single path
         for (buff, r) in calc_buffer
             .iter_mut()
             .zip((&mut rng).sample_iter::<f64, _>(StandardNormal))
@@ -73,118 +138,10 @@ pub fn rust_mc_pricer(mut df: DataFrame, rate: f64, n_sims: i32) -> PolarsResult
         for (buff, d) in calc_buffer.iter_mut().zip(discount_rate.iter()) {
             *buff *= d;
         }
-        // update price with sim result
-        for (price, b) in option_prices.iter_mut().zip(calc_buffer.iter()) {
+        // update price with path result
+        for (price, b) in price_buffer.iter_mut().zip(calc_buffer.iter()) {
             *price += b;
         }
     }
-    // normalize by n_sims
-    for price in option_prices.iter_mut() {
-        *price /= n_sims as f64;
-    }
-    let mut output: Series = option_prices.into_iter().collect();
-    output.rename("option_price");
-    df.with_column(output)?;
-    Ok(df)
-}
-
-pub fn rust_par_mc_pricer(mut df: DataFrame, rate: f64, n_sims: i32) -> PolarsResult<DataFrame> {
-    let n_assets = df.height();
-    let sigma: Vec<f64> = df
-        .column("sigma")?
-        .f64()?
-        .into_iter()
-        .map(Option::unwrap)
-        .collect();
-    let strike: Vec<f64> = df
-        .column("strike")?
-        .f64()?
-        .into_iter()
-        .map(Option::unwrap)
-        .collect();
-    let asset_price: Vec<f64> = df
-        .column("asset_price")?
-        .f64()?
-        .into_iter()
-        .map(Option::unwrap)
-        .collect();
-    let maturity: Vec<f64> = df
-        .column("maturity")?
-        .f64()?
-        .into_iter()
-        .map(Option::unwrap)
-        .collect();
-    let sqrt_maturity: Vec<f64> = maturity.iter().map(|x| x.sqrt()).collect();
-    // mean_drift = (rate - 0.5 * sigma**2) * maturity
-    let mean_drift: Vec<f64> = sigma
-        .iter()
-        .zip(maturity.iter())
-        .map(|(sig, mat)| mat * (rate - 0.5 * sig * sig))
-        .collect();
-    // discount_rate = ((-rate).lit() * col("maturity")).exp()
-    let discount_rate: Vec<f64> = maturity.iter().map(|mat| (-rate * mat).exp()).collect();
-    let n_cores = 8;
-    let n_loops = n_sims / n_cores;
-    // calc_buffer and option_prices are buffers to store calculations through sims
-    let mut option_prices = (0..n_cores)
-        .into_par_iter()
-        .map(|_| {
-            let mut rng = thread_rng();
-            let mut calc_buffer: Vec<f64> = vec![0.0; n_assets];
-            let mut price_buffer: Vec<f64> = vec![0.0; n_assets];
-            for _ in 0..n_loops {
-                // fill in rng values for single sim
-                for (buff, r) in calc_buffer
-                    .iter_mut()
-                    .zip((&mut rng).sample_iter::<f64, _>(StandardNormal))
-                {
-                    *buff = r;
-                }
-                // let rand_walk = col("sigma") * col("maturity").sqrt() * z.lit();
-                for (buff, m) in calc_buffer.iter_mut().zip(sqrt_maturity.iter()) {
-                    *buff *= m;
-                }
-                for (buff, s) in calc_buffer.iter_mut().zip(sigma.iter()) {
-                    *buff *= s;
-                }
-                // let paths = col("asset_price") * (mean_drift + rand_walk).exp();
-                for (buff, m) in calc_buffer.iter_mut().zip(mean_drift.iter()) {
-                    *buff += m;
-                    *buff = (*buff).exp();
-                }
-                for (buff, a) in calc_buffer.iter_mut().zip(asset_price.iter()) {
-                    *buff *= a;
-                }
-                // payoff = discount_rate * (paths - col("strike")).clip_min(AnyValue::Float64(0.0));
-                for (buff, s) in calc_buffer.iter_mut().zip(strike.iter()) {
-                    *buff -= s;
-                    *buff = (*buff).max(0.0);
-                }
-                for (buff, d) in calc_buffer.iter_mut().zip(discount_rate.iter()) {
-                    *buff *= d;
-                }
-                // update price with sim result
-                for (price, b) in price_buffer.iter_mut().zip(calc_buffer.iter()) {
-                    *price += b;
-                }
-            }
-            price_buffer
-        })
-        .reduce(
-            || vec![0.0; n_assets],
-            |a: Vec<f64>, b: Vec<f64>| {
-                a.iter()
-                    .zip(b.iter())
-                    .map(|(elem_a, elem_b)| elem_a + elem_b)
-                    .collect()
-            },
-        );
-    // normalize by n_sims
-    for price in option_prices.iter_mut() {
-        *price /= (n_loops * n_cores) as f64;
-    }
-    let mut output: Series = option_prices.into_iter().collect();
-    output.rename("option_price");
-    df.with_column(output)?;
-    Ok(df)
+    price_buffer
 }
