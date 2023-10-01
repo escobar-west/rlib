504838b258caa781edad49c9a554b34fe83c0663
escobar-west <victorv.suriel@gmail.com>
2023-09-30 23:18:49 -0400

    change n_sims to n_paths

--- src/lib.rs
+++ src/lib.rs
@@ -3,15 +3,15 @@ use pyo3::prelude::*;
 use pyo3_polars::{error::PyPolarsErr, PyDataFrame};
 
 #[pyfunction]
-fn rust_mc_pricer(df: PyDataFrame, rate: f64, n_sims: i32) -> PyResult<PyDataFrame> {
-    let rdf = rust_pricer::rust_mc_pricer(df.into(), rate, n_sims).map_err(PyPolarsErr::from)?;
+fn rust_mc_pricer(df: PyDataFrame, rate: f64, n_paths: i32) -> PyResult<PyDataFrame> {
+    let rdf = rust_pricer::rust_mc_pricer(df.into(), rate, n_paths).map_err(PyPolarsErr::from)?;
     Ok(PyDataFrame(rdf))
 }
 
 #[pyfunction]
-fn rust_par_mc_pricer(df: PyDataFrame, rate: f64, n_sims: i32) -> PyResult<PyDataFrame> {
+fn rust_par_mc_pricer(df: PyDataFrame, rate: f64, n_paths: i32) -> PyResult<PyDataFrame> {
     let rdf =
-        rust_pricer::rust_par_mc_pricer(df.into(), rate, n_sims).map_err(PyPolarsErr::from)?;
+        rust_pricer::rust_par_mc_pricer(df.into(), rate, n_paths).map_err(PyPolarsErr::from)?;
     Ok(PyDataFrame(rdf))
 }
 
